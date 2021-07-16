17f48c5d7f7d6ca6e0121c0d608e1261a0743a92
Lamonin <maze.zte@gmail.com>
2021-07-16 15:22:07 +0900

    Рабочее движение персонажа, относительно корректные анимации

--- engine/LAEngine.pas
+++ engine/LAEngine.pas
@@ -1,5 +1,4 @@
 ﻿unit LAEngine;
-interface
 uses GraphWPF, WPFObjects, Timers;
 type
   spriteInfo=record
@@ -7,42 +6,51 @@ type
     speed:integer; //Скорость анимации
     isLoop:boolean; //Зациклена ли анимация
     end;
+    
   LSprite = class
     private
     anims:Dictionary<string, spriteInfo>; //Все анимации по их именам
     defaultAnim:string; //Имя стандартной анимации
     curAnim:spriteInfo; //Текущая анимация
-    sprite:PictureWPF;
+    sprite, tsprite:PictureWPF;
+    position:Point;
     
     updater:Timer;
     frameNum:Integer; //Номер текущего кадра анимации
     
     procedure ChangeSprite();
     begin
-      var t := sprite;
-      sprite:= new PictureWPF(t.LeftTop, curAnim.frames[frameNum]);
-      t.Destroy();
+      tsprite := sprite;
+      sprite:= new PictureWPF(position, curAnim.frames[frameNum]);
+      tsprite.Destroy;
     end;
     
     //Обновление кадра изображения
     procedure UpdateFrame();
     begin
-      if (frameNum<curAnim.frames.Length) then
+      if (frameNum<curAnim.frames.Length-1) then
         frameNum+=1
       else
         if not (curAnim.isLoop) then begin
           PlayAnim(defaultAnim);
           exit;
         end
-        else
-          frameNum:=0;
+        else frameNum:=0;
+        
       ChangeSprite();
+    end;
+    
+    procedure SetPos(pos:Point);
+    begin
+      sprite.MoveTo(pos.X, pos.Y);
+      position := pos;
     end;
     
     public
     ///Конструктор с инициализацией стандартной анимации с обычными параметрами
     constructor Create(x,y:integer; aname:string; frames:array of string);
     begin
+      x := x * 48; y := y * 48;
       anims := new Dictionary<string, spriteInfo>();
       defaultAnim:=aname;
       AddAnim(aname, frames, 160, True); //Обычно анимация зациклена
@@ -52,6 +60,7 @@ type
     ///Конструктор с инициализацией стандартной анимации
     constructor Create(x,y:integer; aname:string; frames:array of string; speed:integer; looped:boolean);
     begin
+      x := x * 48; y := y * 48;
       anims := new Dictionary<string, spriteInfo>();
       defaultAnim:=aname;
       AddAnim(aname, frames, speed, looped);
@@ -68,33 +77,60 @@ type
     
     procedure PlayAnim(aname:string);
     begin
+      
       curAnim := anims[aname];
       if (updater <> nil) then updater.Stop();
       frameNum := 0;
-      if (curAnim.frames.Length=1) then begin
-        ChangeSprite();
-      end
-      else begin
+      if (curAnim.frames.Length>1) then begin
         updater := new Timer(curAnim.speed, UpdateFrame);
         updater.Start();
       end;
+      
+      ChangeSprite();
     end;
+    
+    ///Устанавливает позицию спрайта
+    property Pos: Point write SetPos;
   end;
   
   ///Загружает спрайт с именем sname.
-  function LoadSprites(sname:string):array of string;
-  ///Загружает последовательность спрайтов с именем sname и номерами от 0 до count.
-  function LoadSprites(sname:string; count:integer):array of string;
+  function LoadSprite(sname:string):array of string;
+  begin
+    Result := new string[1];
+    Result[0] := 'img/'+sname+'.png';
+  end;
   
+  ///Загружает последовательность спрайтов с именем sname и номерами от 1 до count.
+  function LoadSprites(sname:string; count:integer):array of string;
+  begin
+    Result := new string[count];
+    for var i:= 0 to count-1 do begin
+      Result[i] := 'img/'+sname+(i+1)+'.png';
+    end;
+  end;
+  
+  type
   ///Класс игрока в "мире".
   PlayerWorld = class
     private
+    point:RectangleWPF;
+    position:record x,y:integer end;
     sprite:LSprite;
+    moveTimer, idleTimer:Timer;
+    
     public
     constructor Create(x,y:integer);
     begin
-      //Инициализация изображений
-      sprite := new LSprite(100, 100, 'idledown', LoadSprites('player/down2'), 100, false);
+      position.x := x; position.y := y;
+      point := new RectangleWPF(x*48, y*48, 4,4,Colors.Black);
+      point.Visible := false;
+      
+      //Инициализация изображений игрока
+      sprite := new LSprite(x, y, 'idledown', LoadSprite('player/down2'), 160, false);
+      sprite.AddAnim('idleleft', LoadSprite('player/left3'), 160, true);
+      sprite.AddAnim('idleright', LoadSprite('player/right1'), 160, true);
+      sprite.AddAnim('idleup', LoadSprite('player/up2'), 160, true);
+      
       sprite.AddAnim('walkleft', LoadSprites('player/left', 4), 160, true);
       sprite.AddAnim('walkright', LoadSprites('player/right', 4), 160, true);
       sprite.AddAnim('walkup', LoadSprites('player/up', 4), 160, true);
@@ -102,28 +138,43 @@ type
       
       sprite.PlayAnim('idledown');
       //*************************
+      
+      //Обновляем покадрово позицию визуального представления игрока
+      OnDrawFrame += procedure(dt:real) -> begin
+        sprite.Pos := point.LeftTop;
+      end;
+    end;
+    
+    procedure MoveOn(x,y:integer; dir:string);
+    begin
+      writeln(Objects.Count);
+      //Не обрабатываем движение, если персонаж уже идёт
+      if (moveTimer<>nil) and (moveTimer.Enabled) then exit;
+      if (idleTimer<>nil) then idleTimer.Stop();
+      //Обрабатываем "поворот" игрока, включая соответствующую анимацию
+      sprite.PlayAnim('walk'+dir);
+      position.x := x; position.y := y;
+      point.AnimMoveTo(x*48, y*48, 0.5);
+      //Таймер нужен чтобы игрок не двигался с бесконечным ускорением
+      moveTimer := new Timer(480, procedure()->
+      begin
+        idleTimer := new Timer(100, procedure()-> 
+        begin
+          sprite.PlayAnim('idle'+dir);
+          idleTimer.Stop();
+        end);
+        idleTimer.Start();
+        moveTimer.Stop();
+      end);
+      moveTimer.Start();
     end;
     
     procedure Finalize(); override;
     begin
       
     end;
+    
+    property GetX: integer read position.x;
+    property GetY: integer read position.y;
   end;
-  
-  
-  
-implementation
-function LoadSprites(sname:string):array of string;
-begin
-  Result := new string[1];
-  Result[0] := 'img/'+sname+'.png';
-end;
-
-function LoadSprites(sname:string; count:integer):array of string;
-begin
-  Result := new string[count];
-  for var i:= 0 to count-1 do begin
-    Result[i] := 'img/'+sname+(i+1)+'.png';
-  end;
-end;
 end.
--- main.pas
+++ main.pas
@@ -2,24 +2,30 @@
 uses GraphWPF, WPFObjects;
 uses LAEngine in 'engine/LAEngine.pas';
 
+var player:PlayerWorld;
+
+//Процедура вызывается каждый раз при отрисовке
 procedure OnDraw(dt:real);
 begin
 end;
 
+//Обработка ввода пользователя
 procedure KeyDown(k: Key);
 begin
+  if (k = Key.W) or (k = key.Up) then
+    player.MoveOn(player.GetX,player.GetY-1, 'up')
+  else if (k = Key.S) or (k = key.Down) then
+    player.MoveOn(player.GetX,player.GetY+1, 'down')
+  else if (k = Key.A) or (k = key.Left) then
+    player.MoveOn(player.GetX-1,player.GetY, 'left')
+  else if (k = Key.D) or (k = key.Right) then
+    player.MoveOn(player.GetX+1,player.GetY, 'right')
 end;
 
 begin
   
+  player := new PlayerWorld(0, 0);
   
-  
-  OnDrawFrame := OnDraw;
+  OnDrawFrame += OnDraw;
   OnKeyDown := KeyDown;
-//  while (true) do begin
-//    Redraw(procedure()-> 
-//    begin
-//       
-//    end);
-//  end;
 end.
